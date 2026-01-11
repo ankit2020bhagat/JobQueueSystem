8514494d8272c8b3bf185f1ac93e0d9b99b12d69
ankit2020bhagat <animusk7765@gmail.com>
2026-01-11 20:31:32 +0530

    initial commit

---
+++ job_queue/job_queue/src/test/java/com/example/job_queue/JobQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
